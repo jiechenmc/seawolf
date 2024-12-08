5c3a92358632e3cf270ffee866ed6068e45fc0a0
Kevin Cao <kevin.cao.4@stonybrook.edu>
2024-12-08 02:09:18 -0500

    made server use cors

--- p2p/internal/api/api.go
+++ p2p/internal/api/api.go
@@ -10,6 +10,22 @@ type API struct {
     rpcServer *rpc.Server
 }
 
+func enableCORS(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        // Set CORS headers
+        w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins or set specific origin
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS") // Allow methods
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization") // Allow headers
+
+        // Handle preflight requests (OPTIONS request)
+        if r.Method == "OPTIONS" {
+            return
+        }
+
+        next.ServeHTTP(w, r)
+    })
+}
+
 func APIServer() *API {
     //Create interface for frontend
     p2pService := new(P2PService)
@@ -21,7 +37,8 @@ func APIServer() *API {
 }
 
 func (a *API) Start(listenAddr string) {
-    http.HandleFunc("/rpc", a.rpcServer.ServeHTTP)
+    // http.HandleFunc("/rpc", a.rpcServer.ServeHTTP)
+    http.Handle("/rpc", enableCORS(http.HandlerFunc(a.rpcServer.ServeHTTP)))
     err := http.ListenAndServe(listenAddr, nil)
     if err != nil {
         log.Printf("Error starting server. %v\n", err)
